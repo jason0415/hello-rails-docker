8b40fc9eb526cfbe5481a84c94518bec3fe1db13
jason <jason.liu@bigcommerce.com>
2015-07-15 13:01:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRailsDocker::Application.config.secret_token = '4f27c855b79c93adb4525b0d1dce45cb985f8014ae82e9903292262e3f095e1fbbd6c06d3d49dbeb3ab7d50586d4ea621232098967dd2bf016bb83569bf9d5a7'
